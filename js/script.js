c1ade850f5fe3cf6e2224e6927d62552a97caf3f
DhruvMakani <124004385+DhruvMakani@users.noreply.github.com>
2024-11-12 22:30:07 -0500

    Created Car Objects

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Car objects
+let myCar = {
+  model: "Vintage Mustang",
+  brand: "Ford",
+  color: "Green",
+  scale: "1:18",
+  price: "$33.60",
+  material: "Die-cast",
+  SKU: "31166-MAI-GREEN",
+  UPC: "764072005706",
+  packaging: "Window Box",
+  year: 1965,
+  weight: "500g",
+  availability: "In stock"
+};
