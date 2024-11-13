53b17057caa4d649bfe4aca81a514a527221a248
DhruvMakani <124004385+DhruvMakani@users.noreply.github.com>
2024-11-12 22:30:53 -0500

    Added function to display car details

--- js/script.js
+++ js/script.js
@@ -13,3 +13,21 @@ let myCar = {
   weight: "500g",
   availability: "In stock"
 };
+
+// Function to display car details
+function showCarDetails() {
+  document.getElementById("carDetails").innerHTML = `
+      <p>Brand: ${myCar.brand}</p>
+      <p>Model: ${myCar.model}</p>
+      <p>Color: ${myCar.color}</p>
+      <p>Scale: ${myCar.scale}</p>
+      <p>SKU: ${myCar.SKU}</p>
+      <p>UPC: ${myCar.UPC}</p>
+      <p>Year: ${myCar.year}</p>
+      <p>Weight: ${myCar.weight}</p>
+      <p>Material: ${myCar.material}</p>
+      <p>Price: ${myCar.price}</p>
+      <p>packaging: ${myCar.packaging}</p>
+      <p>Availability: ${myCar.availability}</p>
+  `;
+}
